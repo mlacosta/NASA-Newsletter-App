80ad0b31bc9d09f0a11e44606654e353e71a3ac8
mlacosta <marianoacosta.003@gmail.com>
2020-06-11 10:59:32 -0300

    added apd functionality

--- resources/JS/script.js
+++ resources/JS/script.js
@@ -1,2 +1,21 @@
-const apiKey = 'ZrlOrpRipBYYDVLJLcYPqlbAjToE2UB5bVL4CaBx'
+const apiKey = 'ZrlOrpRipBYYDVLJLcYPqlbAjToE2UB5bVL4CaBx';
+const apdURL = 'https://api.nasa.gov/planetary/apod'
+const apdBtn = document.getElementById('apd');
+const apdImg = document.getElementById('apd_img');
 
+const getApd = async () =>{
+
+    try{
+        const endpoint = `${apdURL}?api_key=${apiKey}`
+        const response = await fetch(endpoint);
+        if (response.ok){
+            const jsonResponse = await response.json();
+            apdImg.src = jsonResponse['url'];
+        }
+    }catch(e){
+        alert(error);
+    }
+    
+}
+
+apdBtn.addEventListener('click',getApd)
