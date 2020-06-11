bbb02e1c39079fa6358c905fc8369dec7b3f387e
mlacosta <marianoacosta.003@gmail.com>
2020-06-11 11:57:50 -0300

    still improving

--- resources/JS/script.js
+++ resources/JS/script.js
@@ -2,6 +2,11 @@ const apiKey = 'ZrlOrpRipBYYDVLJLcYPqlbAjToE2UB5bVL4CaBx';
 const apdURL = 'https://api.nasa.gov/planetary/apod'
 const apdBtn = document.getElementById('apd');
 const apdImg = document.getElementById('apd_img');
+const apdInfo = document.getElementById('apd_info');
+const cancelBtn = document.getElementById('cancel');
+const rightBox = document.getElementById('right-box');
+const main = document.querySelector('.main');
+const restoreBtn = document.getElementById('restore');
 
 const getApd = async () =>{
 
@@ -11,6 +16,9 @@ const getApd = async () =>{
         if (response.ok){
             const jsonResponse = await response.json();
             apdImg.src = jsonResponse['url'];
+            apdImg.style.display = 'Block';
+            apdInfo.style.display = 'Block';
+            apdInfo.innerHTML = jsonResponse['explanation'];
         }
     }catch(e){
         alert(error);
@@ -19,3 +27,14 @@ const getApd = async () =>{
 }
 
 apdBtn.addEventListener('click',getApd)
+cancelBtn.addEventListener('click',()=>{
+    rightBox.style.display = 'None';
+    main.style.display = 'block';
+    restoreBtn.style.display = 'block';
+})
+
+restoreBtn.addEventListener('click',()=>{
+    rightBox.style.display = 'Block';
+    main.style.display = 'Grid';
+    event.target.style.display = 'None';
+})
